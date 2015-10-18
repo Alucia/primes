32754659792d70b6095af1a9a5866aa23dda67f8
Alucia <analu1478@gmail.com>
2015-10-18 12:10:53 -0500

    add primes responsive

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+
+    if (scroll >= 600) {        
+    	$(".header").css("background-color", "#0dc0c0");
+    } else {
+       $(".header").css("background-color", "transparent");
+    }
+});
