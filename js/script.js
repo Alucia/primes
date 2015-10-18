76d1f64781487306400e0f60501a22df6cf1306b
Alucia <analu1478@gmail.com>
2015-10-18 13:26:10 -0500

    add portafolio

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).scroll(function () {
-    var scroll = $(this).scrollTop();
-
-    if (scroll >= 600) {        
-    	$(".header").css("background-color", "#0dc0c0");
-    } else {
-       $(".header").css("background-color", "transparent");
-    }
-});
